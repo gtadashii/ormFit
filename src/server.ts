a71bbb05896e328ce13c78045bfafe26a889a5ff
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-07-28 19:47:44 -0300

    Configure Docker containers and typeorm

--- src/server.ts
+++
@@ -1,4 +0,0 @@
-import express from 'express'; // diferente do const express = require...
-const app = express();
-
-app.listen(3333, () => console.log("Hello ormFit"));
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import "../typeorm";
+
+const app = express();
+
+app.listen(3333, () => console.log("Server running 🚀"));
